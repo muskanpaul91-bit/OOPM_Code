16b5bbcae0c00e813bd1378e6bcd7307aea95ea5
muskanpaul91-bit <muskanpaul91@gmail.com>
2025-11-29 12:05:27 +0530

    Add files via upload

---
+++ creating_printing.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "Oriental College of Technology";
+    cout<<str<<endl;
+    int len = str.length();
+    cout<<"Length of string = "<<len<<endl;
+ return 0;
+}
